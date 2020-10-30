a143a38def85d52ddcc23f397aacf6be8dca695f
Adam-Woznica <73635304+Adam-Woznica@users.noreply.github.com>
2020-10-30 10:15:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+
 <?php
-echo "tak";
+
+$conn = new mysqli("sql7.freemysqlhosting.net","sql7373514","sql7373514","8gllrQ1aUJ");
+$sql = "SELECT * FROM pracownicy";
+$wynik = mysqli_query($conn, $sql);
+
+
+    echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th>');
+
+    while($wiersz=mysqli_fetch_assoc($wynik)){
+        echo('<tr>');
+        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+
+
 ?>
