838a9bb5ad0aa7419798330dc4fb748d7b4bf79a
Tomjand <domagala.tomasz@gmail.com>
2020-11-06 09:58:21 +0100

    Create pracownicy_organizacja.php

---
+++ pracownicy_organizacja.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <h1>Adam Woźnica</h1>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-Adam-Woznica">EXIT</a>
+    
+    <div class="nav">
+         <a href="index.php">Strona Główna</a>   
+         <a href="pracownicy_organizacja.php">Pracow. i Org.</a>   
+         <a href="funkcje_agregujace.php">Funkcje Agregujace</a>   
+    </div>
+    
+<?php
+echo("tu jestem");
+
+?>
+</body>
+</html>
