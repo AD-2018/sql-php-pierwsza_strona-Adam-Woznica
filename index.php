eba1853346d62f3ad94a8112034cc364976014f3
Adam-Woznica <73635304+Adam-Woznica@users.noreply.github.com>
2020-11-06 09:47:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ echo('</table>');
   
       $sql ="select avg(zarobki),nazwa_dzial from pracownicy,organizacja where id_org=dzial group by dzial having avg(zarobki)<40"; 
       echo("<h3>zadanie2</h3>"); 
-    echo($sql);
+      echo("<h4>$sql</h4>");
       $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
 echo ("<tr><th>avg(zarobki)</th><th>nazwa_dzial</th></tr>"); 
@@ -59,7 +59,7 @@ echo('</table>');
   
         $sql ="select imie,zarobki from pracownicy where imie not like '%a' having zarobki<40"; 
 echo("<h3>zadanie3</h3>"); 
-echo($sql);
+echo("<h4>$sql</h4>");
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
 echo ("<tr><th>imie</th><th>zarobki</th></tr>"); 
@@ -73,7 +73,7 @@ echo('</table>');
   
           $sql ="select nazwa_dzial from pracownicy,organizacja where id_org=dzial and (dzial=2 or dzial=3) group by dzial"; 
 echo("<h3>zadanie4</h3>"); 
-echo($sql);
+echo("<h4>$sql</h4>");
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
 echo ("<tr><th>nazwa_dzial</th></tr>"); 
