fc698ee6da0c4438eecdbfeb95f7107d65b3ad45
Tomjand <domagala.tomasz@gmail.com>
2020-11-06 09:57:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,18 @@
 <html>
 <head>
 <link rel="stylesheet" href="style.css">
-<h1>Adam Woźnica</h1>
-<a href=https://github.com/AD-2018/sql-php-pierwsza_strona-Adam-Woznica>EXIT</a>
 </head>
 <body>
+    
+    <h1>Adam Woźnica</h1>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-Adam-Woznica">EXIT</a>
+    
+    <div class="nav">
+         <a href="index.php">Strona Główna</a>   
+         <a href="pracownicy_organizacja.php">Pracow. i Org.</a>   
+         <a href="funkcje_agregujace.php">Funkcje Agregujace</a>   
+    </div>
+    
 <?php
 $servername = "sql7.freemysqlhosting.net";
 $username = "sql7373516";
