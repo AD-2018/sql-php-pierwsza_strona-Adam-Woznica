48760aad9b7e1428979f2457f6591e7d937e55b4
Adam-Woznica <73635304+Adam-Woznica@users.noreply.github.com>
2020-11-06 13:00:46 +0100

    Update pracownicy_organizacja.php

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -86,24 +86,6 @@ echo("</tr>");
 }
 echo('</table>');
 
-$sql ="select imie,dzial from pracownicy where dzial=1 or dzial=4";
-echo("<h3>Czas tyka tik tak tik tak</h3>");
-echo("<h4>$sql</h4>");
-$result = mysqli_query($conn, $sql);
-
-echo('<table border="1" class="tabela"');
-echo ("<tr><th>imie</th><th>dzial</th></tr>");
-while($row=mysqli_fetch_assoc($result)){
-echo("<tr>");
-    echo("<tr>");
-    echo("<td>".$row['imie']."</td><td>".$row['dzial']."</td>");
-    echo("<tr>");
-echo("</tr>");
-}
-echo('</table>');
-
-
-
 ?>
 </body>
 </html>
