0c00cd037dc6a11470d80f40a30c62fef0be2db3
Adam-Woznica <73635304+Adam-Woznica@users.noreply.github.com>
2020-11-11 16:39:35 +0100

    Update pracownicy_organizacja.php

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -103,7 +103,7 @@ echo("</tr>");
 }
 echo('</table>');
 
-echo("<h2>Sortowanie</h2>")
+echo("<h2>Sortowanie</h2>");
 
 $sql ="select imie,nazwa_dzial from pracownicy,organizacja where id_org=dzial and imie not like '%a'";
 echo("<h3>Zadanie 5</h3>");
@@ -216,6 +216,25 @@ echo("<tr>");
 echo("</tr>");
 }
 echo('</table>');
+
+echo("<h2>LIMIT</h2>");
+
+$sql ="select * from pracownicy,organizacja where id_org=dzial";
+echo("<h3>Czas tyka tik tak tik tak</h3>");
+echo("<h4>$sql</h4>");
+$result = mysqli_query($conn, $sql);
+
+echo('<table border="1" class="tabela"');
+echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th><th>id_org</th><th>nazwa_dzial</th></tr>");
+while($row=mysqli_fetch_assoc($result)){
+echo("<tr>");
+    echo("<tr>");
+    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td><td>".$row['id_org']."</td><td>".$row['nazwa_dzial']."</td>");
+    echo("<tr>");
+echo("</tr>");
+}
+echo('</table>');
+
 
 
 ?>
