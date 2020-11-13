bbb49f829b2813352f85a0675fa8d9628278ccca
Adam-Woznica <73635304+Adam-Woznica@users.noreply.github.com>
2020-11-13 14:27:07 +0100

    Update data_czas.php

--- data_czas.php
+++ data_czas.php
@@ -25,7 +25,7 @@ $dbname = "michael-musiol_pracownicy_organizacja";
 $conn= new mysqli($servername,$username,$password,$dbname);
 
 $sql ="select * from pracownicy,organizacja where id_org=dzial";
-echo("<h3>wrr</h3>");
+echo("<h3>Za dużo tych zadań</h3>");
 echo("<h4>$sql</h4>");
 $result = mysqli_query($conn, $sql);
 
@@ -40,11 +40,94 @@ echo("</tr>");
 }
 echo('</table>');
 
-
+$sql ="select imie,year(curdate())-year(data_urodzenia) as wiek from pracownicy,organizacja where id_org=dzial and nazwa_dzial='serwis'"; 
+echo("<h3>Zadanie 1</h3>");
+echo("<h4>$sql</h4>");
+$result = mysqli_query($conn, $sql);  
+echo('<table border="1" class="tabela"'); 
+echo ("<tr><th>imie</th><th>wiek</th></tr>"); 
+while($row=mysqli_fetch_assoc($result)){ 
+  echo("<tr>");     
+  echo("<tr>");     
+  echo("<td>".$row['imie']."</td><td>".$row['wiek']."</td>");     
+  echo("<tr>"); 
+  echo("</tr>"); } 
+echo('</table>'); 
+              
+    $sql ="select sum(year(curdate())-year(data_urodzenia)) as suma_lat from pracownicy"; 
+echo("<h3>Zadanie 2 </h3>");
+echo("<h4>$sql</h4>");
+$result = mysqli_query($conn, $sql);  
+echo('<table border="1" class="tabela"'); 
+echo ("<tr><th>suma_lat</th></tr>"); 
+while($row=mysqli_fetch_assoc($result)){ 
+  echo("<tr>");     
+  echo("<tr>");     
+  echo("<td>".$row['suma_lat']."</td>");     
+  echo("<tr>"); 
+  echo("</tr>"); } 
+echo('</table>'); 
+              
+    $sql ="select sum(year(curdate())-year(data_urodzenia)) as suma_lat from pracownicy,organizacja where id_org=dzial and nazwa_dzial='handel'"; 
+echo("<h3>Zadanie 3</h3>");
+echo("<h4>$sql</h4>");
+$result = mysqli_query($conn, $sql);  
+echo('<table border="1" class="tabela"'); 
+echo ("<tr><th>suma_lat</th></tr>"); 
+while($row=mysqli_fetch_assoc($result)){ 
+  echo("<tr>");     
+  echo("<tr>");     
+  echo("<td>".$row['suma_lat']."</td>");     
+  echo("<tr>"); 
+  echo("</tr>"); } 
+echo('</table>'); 
+    
+       $sql ="select sum(year(curdate())-year(data_urodzenia)) as suma_lat from pracownicy where imie like '%a'"; 
+echo("<h3>Zadanie 4</h3>");
+echo("<h4>$sql</h4>");
+$result = mysqli_query($conn, $sql);  
+echo('<table border="1" class="tabela"'); 
+echo ("<tr><th>suma_lat</th></tr>"); 
+while($row=mysqli_fetch_assoc($result)){ 
+  echo("<tr>");     
+  echo("<tr>");     
+  echo("<td>".$row['suma_lat']."</td>");     
+  echo("<tr>"); 
+  echo("</tr>"); } 
+echo('</table>'); 
+        
+        $sql ="select sum(year(curdate())-year(data_urodzenia)) as suma_lat from pracownicy where imie not like '%a'"; 
+echo("<h3>Zadanie 5</h3>");
+echo("<h4>$sql</h4>");
+$result = mysqli_query($conn, $sql);  
+echo('<table border="1" class="tabela"'); 
+echo ("<tr><th>suma_lat</th></tr>"); 
+while($row=mysqli_fetch_assoc($result)){ 
+  echo("<tr>");     
+  echo("<tr>");     
+  echo("<td>".$row['suma_lat']."</td>");     
+  echo("<tr>"); 
+  echo("</tr>"); } 
+echo('</table>'); 
+    
+        $sql ="select nazwa_dzial,avg(year(curdate())-year(data_urodzenia)) as srednia_lat from pracownicy,organizacja where id_org=dzial group by nazwa_dzial"; 
+echo("<h3>Zadanie 6</h3>");
+echo("<h4>$sql</h4>");
+$result = mysqli_query($conn, $sql);  
+echo('<table border="1" class="tabela"'); 
+echo ("<tr><th>nazwa_dzial</th><th>srednia_lat</th></tr>"); 
+while($row=mysqli_fetch_assoc($result)){ 
+  echo("<tr>");     
+  echo("<tr>");     
+  echo("<td>".$row['nazwa_dzial']."</td><td>".$row['srednia_lat']."</td>");     
+  echo("<tr>"); 
+  echo("</tr>"); } 
+echo('</table>'); 
+    
 
 ?>
 
-<h2>A guziki są piękne      >:|</h2>
+<h2>Dobra styknie tego</h2>
 
 </body>
 </html>
