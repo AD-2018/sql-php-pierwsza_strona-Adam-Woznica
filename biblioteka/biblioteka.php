3b597774f17bfa02b2729e8b56cea0d93cc6cefe
Adam-Woznica <woznica.adam28@gmail.com>
2021-02-25 16:35:42 +0100

    tt

--- biblioteka/biblioteka.php
+++ biblioteka/biblioteka.php
@@ -5,11 +5,18 @@
 </head>
 <body>
     
-<div class="diw">
-    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-Adam-Woznica">EXIT</a>
-    <a class="link" href="../index.php">Strona Główna</a>
-   
-</div>
+<nav>
+        <ul>
+            <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-Adam-Woznica">EXIT</a>
+            <a href="../index.php">Strona Główna</a>
+            <a href="../pracownicy/pracownicy_organizacja.php">Pracow. i Org.</a>
+            <a href="../pracownicy/funkcje_agregujace.php">Funkcje Agregujace</a>
+            <a href="../pracownicy/data_czas.php">Data i Czas</a>
+            <a href="../pracownicy/formularz.html">Formularze</a>
+            <a href="../pracownicy/danedobazy.php">+ i -</a>
+            <a href="biblioteka/biblioteka.php">Biblioteka</a>
+        </ul>
+    </nav>
     
 <?php
 echo("<h1>Adam Woźnica</h1>");
