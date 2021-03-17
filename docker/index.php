44476343b1d3f1f2a1253384fabdc150df8261e0
Adam-Woznica <woznica.adam28@gmail.com>
2021-03-17 18:21:17 +0100

    XDD

--- docker/index.php
+++ docker/index.php
@@ -1,8 +1,39 @@
 <html>
 <head>
 <a href="../index.php">Strona Główna</a>
+<body>
+
+
 <?php
 
+
+$servername = "localhost/phpmyadmin";
+$username = "root";
+$password = "root";
+$dbname = "tak";
+
+$conn= new mysqli($servername,$username,$password,$dbname);
+
+$sql ="select * from pracownicy";
+echo("<h3>Może Pan mówić co chce, ja jestem dumny z tych guzików</h3>");
+echo("<h4>$sql</h4>");
+$result = mysqli_query($conn, $sql);
+
+echo('<table border="1" class="tabela"');
+echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
+while($row=mysqli_fetch_assoc($result)){
+echo("<tr>");
+    echo("<tr>");
+    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
+    echo("<tr>");
+echo("</tr>");
+}
+echo('</table>');
+
 ?>
+
+
+</body>
+
 
 </html>
