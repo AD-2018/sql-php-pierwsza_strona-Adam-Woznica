750fcdf6d2f7cb8b6d077cebe8593cc07f501fb1
Adam-Woznica <73635304+Adam-Woznica@users.noreply.github.com>
2021-03-17 20:00:17 +0100

    Update index.php

--- docker/index.php
+++ docker/index.php
@@ -1,39 +1,7 @@
-<html>
-<head>
-<a href="../index.php">Strona Główna</a>
-<body>
-
-
 <?php
 
 
-$servername = "db";
-$username = "root";
-$password = "root";
-$dbname = "tom";
-
-$conn= new mysqli($servername,$username,$password,$dbname);
-
-$sql ="select * from pracownicy";
-echo("<h3>Może działa może nie</h3>");
-echo("<h4>$sql</h4>");
-$result = mysqli_query($conn, $sql);
-
-echo('<table border="1" class="tabela"');
-echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
-while($row=mysqli_fetch_assoc($result)){
-echo("<tr>");
-    echo("<tr>");
-    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
-    echo("<tr>");
-echo("</tr>");
-}
-echo('</table>');
 
 ?>
 
 
-</body>
-
-
-</html>
