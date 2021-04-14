86a5e15cc7bf5a4e2465aa9c3f2fdbfddbd52881
Adam-Woznica <woznica.adam28@gmail.com>
2021-04-14 21:36:23 +0200

    f

--- jednorożec/strona4/index.php
+++ jednorożec/strona4/index.php
@@ -12,14 +12,90 @@
       <header>
         <?php
         echo"<h1>Witam</h1>";
-        ?>
+        
+              require_once("../../connect.php");
+
+              $sql ="select * from fryzjer";
+              $wynik = mysqli_query($conn, $sql);
+                  
+                  echo("<br>");
+                  echo("Tabele");
+                  echo("<br>");
+                  echo($sql);
+                  echo('<table border="1">');
+                  echo('<th>id_fryzjer</th><th>fryzjer</th>');
+              
+                  while($wiersz=mysqli_fetch_assoc($wynik))
+                  {
+                      echo('<tr>');
+                      echo('<td>'.$wiersz['id_fryzjer'].'</td>'.'<td>'.$wiersz['fryzjer'].'</td>');
+                      echo('</tr>');
+                  }
+              
+                  echo('</table>');
+                  
+              echo("<br>");
+
+        
+              ?>
+        
       </header>
       <nav>
         2
+        <?php
+              require_once("../../connect.php");
+
+              $sql ="select * from klient";
+              $wynik = mysqli_query($conn, $sql);
+                  
+                  echo("<br>");
+                  echo("Tabele");
+                  echo("<br>");
+                  echo($sql);
+                  echo('<table border="1">');
+                  echo('<th>id_klient</th><th>klient</th>');
+              
+                  while($wiersz=mysqli_fetch_assoc($wynik))
+                  {
+                      echo('<tr>');
+                      echo('<td>'.$wiersz['id_klient'].'</td>'.'<td>'.$wiersz['klient'].'</td>');
+                      echo('</tr>');
+                  }
+              
+                  echo('</table>');
+                  
+              echo("<br>");
+
+        
+              ?>
       </nav>
 
       <main>
         3
+        <?php
+              require_once("../../connect.php");
+
+              $sql = "SELECT id, fryzjer, klient FROM spa, fryzjer, klient WHERE fryzjer.id_fryzjer = spa.id_fryzjer AND klient.id_klient = spa.id_klient";
+              $wynik = mysqli_query($conn, $sql);
+                  
+                  echo("<br>");
+                  echo("Tabele");
+                  echo("<br>");
+                  echo($sql);
+                  echo('<table border="1">');
+                  echo('<th>id</th><th>fryzjer</th><th>klient</th>');
+              
+                  while($wiersz=mysqli_fetch_assoc($wynik))
+                  {
+                      echo('<tr>');
+                      echo('<td>'.$wiersz['id'].'</td>'.'<td>'.$wiersz['fryzjer'].'</td>'.'<td>'.$wiersz['klient'].'</td>');
+                      echo('</tr>');
+                  }
+              
+                  echo('</table>');
+                  
+              echo("<br>");
+                  ?>
       </main>
       <aside>
         5
