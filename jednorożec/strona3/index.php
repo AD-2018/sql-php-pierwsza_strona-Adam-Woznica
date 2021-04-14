26c958c4ad5c1324729010731a7aca185f0ec83b
Adam-Woznica <woznica.adam28@gmail.com>
2021-04-14 21:16:26 +0200

    działa?

--- jednorożec/strona3/index.php
+++ jednorożec/strona3/index.php
@@ -12,14 +12,86 @@
       <header>
         <?php
         echo"<h1>RIP Krzysztof Krawczyk [*]</h1>";
+        require_once("../../connect.php");
+
+        $sql ="select * from prawnik";
+        $wynik = mysqli_query($conn, $sql);
+            
+            echo("<br>");
+            echo("Tabele");
+            echo("<br>");
+            echo($sql);
+            echo('<table border="1">');
+            echo('<th>id_prawnik</th><th>prawnik</th>');
+        
+            while($wiersz=mysqli_fetch_assoc($wynik))
+            {
+                echo('<tr>');
+                echo('<td>'.$wiersz['id_chlop'].'</td>'.'<td>'.$wiersz['pracownik'].'</td>');
+                echo('</tr>');
+            }
+        
+            echo('</table>');
+            
+        echo("<br>");
         ?>
+        
         </header>
       <nav>
         2
+        <?php
+        echo"<h1>Jednorożec tańczy, tańczy, tańczy, tańczy</h1>";
+        require_once("../../connect.php");
+
+        $sql ="select * from sprawa";
+        $wynik = mysqli_query($conn, $sql);
+            
+            echo("<br>");
+            echo("Tabele");
+            echo("<br>");
+            echo($sql);
+            echo('<table border="1">');
+            echo('<th>id_sprawa</th><th>sprawa</th>');
+        
+            while($wiersz=mysqli_fetch_assoc($wynik))
+            {
+                echo('<tr>');
+                echo('<td>'.$wiersz['id_sprawa'].'</td>'.'<td>'.$wiersz['sprawa'].'</td>');
+                echo('</tr>');
+            }
+        
+            echo('</table>');
+            
+        echo("<br>");
+        ?>
       </nav>
 
       <main>
         3
+        <?php
+              require_once("../../connect.php");
+
+              $sql = "SELECT id, prawnik, sprawa FROM kancelaria, prawnik, sprawa WHERE prawnik.id_prawnik = kancelaria.id_prawnik AND sprawa.id_sprawa = kancelaria.id_sprawa";
+              $wynik = mysqli_query($conn, $sql);
+                  
+                  echo("<br>");
+                  echo("Tabele");
+                  echo("<br>");
+                  echo($sql);
+                  echo('<table border="1">');
+                  echo('<th>id</th><th>prawnik</th><th>sprawa</th>');
+              
+                  while($wiersz=mysqli_fetch_assoc($wynik))
+                  {
+                      echo('<tr>');
+                      echo('<td>'.$wiersz['id'].'</td>'.'<td>'.$wiersz['prawnik'].'</td>'.'<td>'.$wiersz['sprawa'].'</td>');
+                      echo('</tr>');
+                  }
+              
+                  echo('</table>');
+                  
+              echo("<br>");
+                  ?>
       </main>
       <aside>
         5
