3c155f77e24fce6d037c36bdaf28ae4db66a37b4
Adam-Woznica <woznica.adam28@gmail.com>
2021-04-19 08:26:18 +0200

    menu

--- index.php
+++ index.php
@@ -8,16 +8,9 @@
     <h1>Adam Woźnica ! </h1>
     <nav>
         <ul>
-            <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-Adam-Woznica">EXIT</a>
-            <a href="index.php">Strona Główna</a>
-            <a href="pracownicy/pracownicy_organizacja.php">Pracow. i Org.</a>
-            <a href="pracownicy/funkcje_agregujace.php">Funkcje Agregujace</a>
-            <a href="pracownicy/data_czas.php">Data i Czas</a>
-            <a href="pracownicy/formularz.html">Formularze</a>
-            <a href="pracownicy/danedobazy.php">+ i -</a>
-            <a href="biblioteka/biblioteka.php">Biblioteka</a>
-            <a href="docker/index.php">DOCKER</a>
-           <a href="jednorożec/indexx.html">Jednorożce</a>
+           <?php 
+           require_once("menu.php");
+           ?>
     </nav>
     
 <?php
