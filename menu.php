ed6ffc24e2c23a110ffb1515fb56f907b93ad4e4
Adam-Woznica <woznica.adam28@gmail.com>
2021-04-19 08:28:28 +0200

    c

--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
 <?php
 <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-Adam-Woznica">EXIT</a>
-<a href="index.php">Strona Główna</a>
+<a href="../index.php">Strona Główna</a>
 <a href="pracownicy/pracownicy_organizacja.php">Pracow. i Org.</a>
 <a href="pracownicy/funkcje_agregujace.php">Funkcje Agregujace</a>
 <a href="pracownicy/data_czas.php">Data i Czas</a>
