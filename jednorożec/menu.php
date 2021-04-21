066157ba6c7d681bc846fa7f249a5ba2fa4a46db
Adam-Woznica <woznica.adam28@gmail.com>
2021-04-21 09:27:57 +0200

    menu

---
+++ jednorożec/menu.php
@@ -0,0 +1,10 @@
+<a href="https://github.com/AD-2018/sql-php-pierwsza_strona-Adam-Woznica">EXIT</a>
+    <a href="../index.php">Strona Główna</a>
+    <a href="Strona1/index.php">Strona1</a>
+    <a href="Strona2/index.php">Strona2</a>
+    <a href="strona3/index.php">Strona3</a>
+    <a href="strona4/index.php">Strona4</a>
+    <a href="strona5/index.php">Strona5</a>
+    <a href="strona6/index.php">Strona6</a>
+    <a href="strona7/index.php">Strona7</a>
+    <a href="strona8/index.php">Strona8</a>
