81f7bfb64bc3ec63fb2e5552153c3a0fbc8394e8
Adam-Woznica <woznica.adam28@gmail.com>
2021-04-21 10:11:13 +0200

    XDDD

---
+++ jednorożec/strona4/fryzjer.php
@@ -0,0 +1,24 @@
+<?php
+echo $_POST['id'];
+
+require_once("../../connect.php");
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "DELETE FROM fryzjer WHERE id_fryzjer=".$_POST['id'];
+
+
+echo $sql;
+
+if ($conn->query($sql) === TRUE) {
+  echo "Record deleted successfully";
+  header('Location: https://woznicaadam.herokuapp.com/jednoro%C5%BCec/Strona4/index.php');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
